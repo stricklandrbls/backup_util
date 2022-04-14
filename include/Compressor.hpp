2a7d47ff3713b3da324dc932a59d8570b279b2b1
stricklandrbls <strickland.rbls@gmail.com>
2022-04-13 22:13:58 -0500

    Spawns a child process for help zip

--- include/Compressor.hpp
+++ include/Compressor.hpp
@@ -5,7 +5,31 @@
 
 class Compressor{
     public:
-        static inline void compress(Content* content_to_compress){
+        static inline void compress(std::vector<Directory*>* content_to_compress, std::size_t& lb, std::size_t& mb, std::size_t& forks){
+            if(fork() == 0){
+                std::string command;
+                for(std::vector<Directory*>::iterator it = content_to_compress->begin(); it <= content_to_compress->begin() + lb; it++){
+                    printf("compressing dir<%p>: %s\n", *it, (*it)->getPath().c_str());
+
+                    command = "zip -rv -7 ";
+                    command += Compressor::getDestinationPath();
+                    command += "/" + (*it)->_dirname() + " " + (*it)->getPath();
+                    system(command.c_str());
+                    Terminal::print(Terminal::success, command);
+                }
+            }
+            else{
+                std::string command;
+                for(std::vector<Directory*>::iterator it = content_to_compress->begin() + mb; it != content_to_compress->end(); it++){
+                    printf("compressing dir<%p>: %s\n", *it, (*it)->getPath().c_str());
+
+                    command = "zip -rv -7 ";
+                    command += Compressor::getDestinationPath();
+                    command += "/" + (*it)->_dirname() + " " + (*it)->getPath();
+                    system(command.c_str());
+                    Terminal::print(Terminal::success, command);
+                }
+            }
 
         };
         static inline void setDestination(std::string* path, std::string* host){
@@ -22,4 +46,5 @@ class Compressor{
         static inline Destination destination_path;
         static inline std::string zip_directory = "zip -r ";
         static inline std::string zip_file = "zip ";
+
 };
--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,17 @@ int main(){
     std::vector<File*>*         file_to_compress = config.getFiles();
 
     std::string command;
-    for(auto* dir : *dir_to_compress){
-
-        command = "zip -rv -7 ";
-        command += Compressor::getDestinationPath();
-        command += "/" + dir->_dirname() + " " + dir->getPath();
-        system(command.c_str());
-        Terminal::print(Terminal::success, command);        
+    std::size_t forks = dir_to_compress->size() / 2;
+    std::size_t lb, mb;
+    if(forks % 2 != 0){
+        lb = forks;
+        mb = forks + 1;
     }
+    else{
+        lb = forks -1;
+        mb = forks;
+    }
+    Compressor::compress(dir_to_compress, lb, mb, forks);
     return 0;
 }
+
